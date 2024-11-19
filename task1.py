d5094c8b97efeeb0abcf59f8cf10186b0e56737e
Shiyizhuanshi <hj1021@ic.ac.uk>
2024-11-19 01:03:16 +0000

    editing task1

--- task1.py
+++ task1.py
@@ -50,17 +50,16 @@ def schedule_job(index, edges):
             edges.remove(edge)
 
 def cost_function(job, scheduled_jobs):
-    cost = 0
-    cur_time = get_cur_time(scheduled_jobs) + p[job]
-    cost = max(0, cur_time - d[job])
-    return cost
+    completion_time = get_cur_time(scheduled_jobs) + p[job]
+    return max(0, completion_time - d[job])
 
 def choose_job_to_schedule(jobs_to_choose, scheduled_jobs):
     min_cost = 999999
     job_to_schedule = -1
     for job in jobs_to_choose:
         cost = cost_function(job, scheduled_jobs)
-        if cost < min_cost:
+        print("job:" + str(job) + " cost:" + str(cost))
+        if cost <= min_cost:
             min_cost = cost
             job_to_schedule = job
     return job_to_schedule
@@ -96,23 +95,48 @@ def schedule_last_jobs(edges, scheduled_jobs):
             job = choose_job_to_schedule(jobs_to_choose, scheduled_jobs)
             print("job to schedule:" + str(job))
             last_jobs.remove(job)
-            scheduled_jobs.append(job)
+            scheduled_jobs.insert(0, job)
             for edge in edges:
                 if edge[0] == job:
                     edges.remove(edge)
 
-schedule_jobs = []
-while(len(schedule_jobs) != 31):
-    print("====================================")
-    print("lengt of schedule_jobs:" + str(len(schedule_jobs)))
-    jobs = get_jobs_without_successor(edges, schedule_jobs)
-    print("jobs without successor:" + str(jobs))
-    if (len(jobs) == 0):
-        schedule_last_jobs(edges, schedule_jobs)
-    else:
-        print("schedule_jobs:" + str(jobs[0]))
-        job = choose_job_to_schedule(jobs, schedule_jobs)
-        schedule_job(job, edges)
-        schedule_jobs.append(job)
-    print("schedule_jobs:" + str(schedule_jobs))
-    print("edges" + str(edges))
+def calculate_total_cost(schedule_jobs):
+    total_cost = 0
+    cur_time = 0
+    for job in schedule_jobs:
+        cur_time += p[job]
+        total_cost += max(0, cur_time - d[job])
+        print("current total cost:" + str(total_cost))
+    return total_cost
+
+def calculate_max_cost(schedule_jobs):
+    max_cost = -999999
+    cur_time = 0
+    for job in schedule_jobs:
+        cur_time += p[job]
+        cur_cost = max(0, cur_time - d[job])
+        if cur_cost > max_cost:
+            max_cost = cur_cost
+    return max_cost
+
+def calculate_best_schedule(egdes_of_jobs):
+    scheduled_jobs = []
+    while(len(scheduled_jobs) != 31):
+        print("====================================")
+        print("lengt of scheduled_jobs:" + str(len(scheduled_jobs)))
+        jobs = get_jobs_without_successor(egdes_of_jobs, scheduled_jobs)
+        print("jobs without successor:" + str(jobs))
+        if (len(jobs) == 0):
+            schedule_last_jobs(egdes_of_jobs, scheduled_jobs)
+        else:
+            job = choose_job_to_schedule(jobs, scheduled_jobs)
+            schedule_job(job, egdes_of_jobs)
+            scheduled_jobs.insert(0, job)
+            print("scheduled job:" + str(job))
+        print("scheduled_jobs:" + str(scheduled_jobs))
+        # print("edges" + str(egdes_of_jobs))
+    return scheduled_jobs
+
+scheduled_jobs = calculate_best_schedule(edges)
+print("Total cost:" + str(calculate_total_cost(scheduled_jobs)))
+print("Max cost:" + str(calculate_max_cost(scheduled_jobs)))
